cfd82df0c32ffb4fd56720ff4b12143d372fd44e
NatashaTheRobot <nmurashev@gmail.com>
2024-11-20 13:10:22 +0530

    Initial Commit

---
+++ RunModelLocally/RunModelLocallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunModelLocallyApp.swift
+//  RunModelLocally
+//
+//  Created by Natasha Murashev on 11/20/24.
+//
+
+import SwiftUI
+
+@main
+struct RunModelLocallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
